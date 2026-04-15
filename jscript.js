412c4868f651f131fcae64ce4289109deeecb740
chiqo404 <270429796+chiqo404@users.noreply.github.com>
2026-04-15 20:26:27 +0400

    calc

--- jscript.js
+++ jscript.js
@@ -1,6 +1,6 @@
-function add(a, b) {return a+b}
-function subtract(a,b){return a-b}
-function multiply(a,b){return a*b}
+function add(a, b) {return +a + +b}
+function subtract(a,b){return +a - +b}
+function multiply(a,b){return a * b}
 function divide(a,b){return a/b}
 let a = 0;
 let op ='';
@@ -11,14 +11,98 @@ function operate(a,op,b){
     if(op == '*') multiply(a,b);
     if(op == '/') divide(a,b);
 }
-const display = document.querySelector("#screen");
+const display = document.querySelector(".new");
+const old = document.querySelector(".old");
 const digits = document.querySelectorAll(".digit");
+const cline = document.querySelector(".cline");
+const clear = document.querySelector(".clear");
+const del = document.querySelector(".delete");
+const operations = document.querySelectorAll(".operation");
+let counter = 0;
+display.textContent = 0;
 digits.forEach(button => {
     button.addEventListener("click", () => {
-        a = button.textContent;
-        const num = document.createElement("number");
-        num.textContent = a;
-        display.append(num);
-        console.log(a);
+        if(op == '='){
+            a = 0;
+            display.textContent = 0;
+            old.textContent = "";
+            op = '';
+        }
+        if(button.textContent == '.'){
+             a = +a + '.';
+             display.textContent = a;
+        }
+        else{
+             a += button.textContent;
+             display.textContent = +a;
+        }
     });
 });
+del.addEventListener("click", () => {
+    if(display.textContent != "ERROR"){
+        a -= (a%10);
+        a = a / 10;
+        display.textContent = +a;
+        console.log(a);
+    }
+});
+cline.addEventListener("click", () =>{
+    if(display.textContent != "ERROR"){
+            a = 0;
+            display.textContent = +a;
+    }
+});
+clear.addEventListener("click", () =>{
+    a = 0;
+    b = 0;
+    display.textContent = 0;
+    old.textContent = "";
+})
+operations.forEach(operation =>{
+operation.addEventListener("click", () =>{
+    if(display.textContent != "ERROR"){
+        let cop = operation.textContent;
+        if(b == 0){
+            op = operation.textContent;
+            old.textContent = +a + op;
+            b = a;
+            a = 0;
+            display.textContent = 0;
+        }
+        else{
+            let result = 0;
+            if(op == '+') result = add(a,b);
+            else if(op == '-')  result = subtract(b,a);
+            else if(op == '×')  result = multiply(a,b);
+            else{
+                if(a == 0) result = "ERROR";
+                else result = divide(b,a);
+            }
+            if(cop == '='){
+                old.textContent = +b + op + +a + '=';
+                display.textContent = result;
+                a = result;
+                b = 0;
+                op = '=';
+            }
+            else{
+                if(result != "ERROR"){
+                    display.textContent = 0;
+                    old.textContent = +result + cop;
+                    op = cop;
+                    b = result;
+                    a = 0;
+                }
+                else{
+                    a = 0;
+                    b = 0;
+                    display.textContent = result;
+                    op = '';
+                    old.textContent = "";
+                }
+            }
+        }
+    }
+});
+});
+
